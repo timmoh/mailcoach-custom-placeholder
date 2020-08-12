407907b96ae88c5dc5f73da6a1c6c0e08fac221e
timmoh <timo@doersching.net>
2020-08-12 15:25:12 +0200

    add PlaceholderFactory

---
+++ database/factories/PlaceholderFactory.php
@@ -0,0 +1,16 @@
+<?php
+use Faker\Generator;
+use Illuminate\Support\Str;
+use Spatie\Mailcoach\Models\EmailList;
+use Timmoh\MailcoachCustomPlaceholder\Models\Placeholder;
+
+$factory->define(Placeholder::class, function (Generator $faker) {
+
+    return [
+        'name' => Str::random(10),
+        'description' => $faker->sentence,
+        'replace_value' => $faker->colorName,
+        'email_list_id' => $faker->randomHtml(),
+        'email_list_id' => fn () => factory(EmailList::class)->create(['uuid' => (string)Str::uuid()]),
+    ];
+});
