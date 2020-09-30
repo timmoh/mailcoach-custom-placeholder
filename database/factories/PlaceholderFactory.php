83e90f555802ec2d54a8d22b3cb4f3b041ef6b6d
timmoh <timo@doersching.net>
2020-09-30 10:38:41 +0200

    delete double fields

--- database/factories/PlaceholderFactory.php
+++ database/factories/PlaceholderFactory.php
@@ -9,7 +9,6 @@ $factory->define(Placeholder::class, function (Generator $faker) {
         'name' => Str::random(10),
         'description' => $faker->sentence,
         'replace_value' => $faker->colorName,
-        'email_list_id' => $faker->randomHtml(),
         'email_list_id' => fn () => factory(EmailList::class)->create(['uuid' => (string)Str::uuid()]),
     ];
 });
